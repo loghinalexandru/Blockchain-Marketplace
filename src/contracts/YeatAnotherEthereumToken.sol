e915c2b8fe08243ac7b2bb87cd6f5d90bc0adbec
loghinalexandru <loghinalexandru61@gmail.com>
2021-01-22 19:23:23 +0200

    Add Custom Token

---
+++ src/contracts/YeatAnotherEthereumToken.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.7.3;
+import "./ERC20.sol";
+
+contract YetAnotherEthereumToken is ERC20 {
+    constructor() ERC20("YetAnotherEthereumToken",  "YAET"){
+        _mint(msg.sender, 1000);
+    }
+}
